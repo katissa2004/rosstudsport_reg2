cb81dc6b87a43834c48a0b65555efa8163879a7e
katissa2004 <125659069+katissa2004@users.noreply.github.com>
2023-10-18 17:53:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ TABLE_KEY = '1C6HTpbHSV0dXUcXN4x-yYiBuVEvAcEU1PB5wd_EaUyI' # the key from link-t
 # TABLE_TO_USE = ''	#put name of the table
 # TABLE_RANGE = ''	#put number of column with names in table
 # EVENT_NUMBER = 0 	#put number from the event url on pandas.bmstu.net
-EMAIL = 'katesolovyova2004@gmail.com'	# put your email on pandas.bmstu.net
-PASSWORD = '221112'	# put your password on pandas.bmstu.net
+EMAIL = ''	# put your email on pandas.bmstu.net
+PASSWORD = ''	# put your password on pandas.bmstu.net
